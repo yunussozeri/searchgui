463c1eeb8ca6e77cbb7ad10dec6e4f8f256a6e3b
yunussozeri <yunus.sozeri.254@gmail.com>
2023-06-07 19:53:24 +0200

    removed fitlered list and filtered result. search method needs some polish, case sensitivity is buggy(absolutely not working lol) docs missing. and for some reason toList()from streams casues compilation error

--- src/main/java/pm2/zugmv/Model.java
+++ src/main/java/pm2/zugmv/Model.java
@@ -14,13 +14,14 @@ import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 
 /**
- * This Class represents the Model Class from MVC-Model.
- * This class reads and stores the data.
- * Furthermore it provides the data to the view and controller classes
+ * This Class represents the Model Class from MVC-Model. This class reads and
+ * stores the data. Furthermore it provides the data to the view and controller
+ * classes
  *
  * @author yunussozeri
  */
 public class Model {
+
     //private List<Row> rows;
     private ObservableList<Row> rows;
 
@@ -28,8 +29,9 @@ public class Model {
         this.rows = FXCollections.observableArrayList();
         readLines(filename);
     }
+
     public Model() throws IOException {
-        
+
         this.rows = FXCollections.observableArrayList();
         readLines("evu_brd_gekuerzt.csv");
     }
@@ -39,36 +41,40 @@ public class Model {
         BufferedReader br = new BufferedReader(new FileReader(filename));
         br.lines().skip(1)
                 .map(line -> line.split(";"))
-                .map((String[] row) -> new Row(row[0],row[1],row[2],row[3],row[4],row[5]))
-                .forEach((Row row)-> rows.add(row));
-    }
-    public List<String> getUnternehmenColumn(){
-        return rows.stream().map(Row::getUnternehmen).toList();
-    }
-    public List<String> getStreetColumn(){
-        return rows.stream().map(Row::getStrasse).toList();
-    }
-    public List<String> getPlzColumn(){
-        return rows.stream().map(Row::getPlz).toList();
-    }
-    public List<String> getOrtColumn(){
-        return rows.stream().map(Row::getOrt).toList();
-    }
-    public List<String> getGueterverkehrColumn(){
-        return rows.stream().map(Row::isGueterverkehr).toList();
-    }
-    public List<String> getPersonenverkehrColumn(){
-        return rows.stream().map(Row::isPersonenverkehr).toList();
+                .map((String[] row) -> new Row(row[0], row[1], row[2], row[3], row[4], row[5]))
+                .forEach((Row row) -> rows.add(row));
     }
 
-    
-    public ObservableList<Row> getRows(){
+    /*public List<String> getUnternehmenColumn() {
+        return rows.stream().map(Row::getUnternehmen).toList();
+    }
+
+    public List<String> getStreetColumn() {
+        return rows.stream().map(Row::getStrasse).toList();
+    }
+
+    public List<String> getPlzColumn() {
+        return rows.stream().map(Row::getPlz).toList();
+    }
+
+    public List<String> getOrtColumn() {
+        return rows.stream().map(Row::getOrt).toList();
+    }
+
+    public List<String> getGueterverkehrColumn() {
+        return rows.stream().map(Row::isGueterverkehr).toList();
+    }
+
+    public List<String> getPersonenverkehrColumn() {
+        return rows.stream().map(Row::isPersonenverkehr).toList();
+    }*/
+
+    public ObservableList<Row> getRows() {
         return rows;
     }
-    
-    
-    public Row getRowAt(int index){
+
+    public Row getRowAt(int index) {
         return rows.get(index);
     }
-    
+
 }
